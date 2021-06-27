1cdd40d0b826f994fd788887e1e0893e34464465
Renan Ferreira <renansantiago2013@hotmail.com>
2021-06-27 15:04:35 -0300

    Adiciona altura para o rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -6,7 +6,7 @@ class Rating extends React.Component {
   render() {
     const { rating } = this.props;
 
-    return <p className="rating">{rating}</p>;
+    return <span className="rating">{rating}</span>;
   }
 }
 
